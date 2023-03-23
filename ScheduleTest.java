628c9b2c3b9bdabbcefa3a7011ba39532bae0443
Rohil Singh Dhillon <64743078+Rohil1710@users.noreply.github.com>
2023-03-23 15:41:02 -0600

    Update ScheduleTest.java

--- ScheduleTest.java
+++ ScheduleTest.java
@@ -143,5 +143,61 @@ public class ScheduleTest {
         assertEquals("startHour was incorrect: ", expResult, result);
 
     }
+    
+  
+    //Basic Tests for Task class to be added to main ScheduleTest file
+
+
+    @Test
+    public void testGetTaskID() {
+    
+        Task task1 = new Task(1, 30, 2, 'Kit feeding');
+          
+        System.out.println("\nTesting getTaskID...");
+        int expResult = 1;
+        int result = task1.getTaskID();
+        assertEquals("taskID was incorrect: ", expResult, result);
+
+    }
+
+    @Test
+    public void testGetTaskDescription() {
+
+        Task task1 = new Task(3, 10, 3, 'Apply burn ointment back');
+
+        System.out.println("\nTesting getTaskDescription...");
+        int expResult = 'Apply burn ointment back';
+        int result = task1.getTaskDescription();
+        assertEquals("taskDescription was incorrect: ", expResult, result);
+
+    }
+
+
+    @Test
+    public void testGetTaskDuration() {
+
+        Task task1 = new Task(10, 5, 2, 'Inspect broken leg');
+
+        System.out.println("\nTesting getTaskDuration...");
+        int expResult = 5;
+        int result = task1.getTaskDuration();
+        assertEquals("taskDescription was incorrect: ", expResult, result);
+
+    }
+
+    @Test
+    public void testGetTaskMaxTime() {
+
+        Task task1 = new Task(9, 25, 1, 'Eyedrops');
+    
+        System.out.println("\nTesting getTaskMaxTime...");
+        int expResult = 25;
+        int result = task1.getStartHour();
+        assertEquals("taskMaxTime was incorrect: ", expResult, result);
+
+    }
+
+
+
 
 }
