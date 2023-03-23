319114cebf90286abec2645d36bb3837878492a8
T-Smithy <113543505+T-Smithy@users.noreply.github.com>
2023-03-23 15:27:44 -0600

    Update ScheduleTest.java
    moved Treatment class tests

--- ScheduleTest.java
+++ ScheduleTest.java
@@ -36,57 +36,6 @@ public class ScheduleTest {
         
     }
         
-    //Basic Tests for Treatment class to be added to main ScheduleTest file
-
-
-    @Test
-    public void testGetTreatmentID() {
-        
-        Treatment treatment1 = new Treatment(1, 6, 1, 0)
-              
-        System.out.println("\nTesting getTreatmentID...");
-        int expResult = 1;
-        int result = treatment1.getTreatmentID();
-        assertEquals("treatmentID was incorrect: ", expResult, result);
-
-    }
-
-    @Test
-    public void testGetAnimalID() {
-
-        Treatment treatment1 = new Treatment(2, 6, 1, 2);
-
-        System.out.println("\nTesting getAnimalID...");
-        int expResult = 6;
-        int result = treatment1.getAnimaltID();
-        assertEquals("animalID was incorrect: ", expResult, result);
-
-    }
-
-
-    @Test
-    public void testGetTaskID() {
-
-        Treatment treatment1 = new Treatment(3, 6, 1, 4);
-
-        System.out.println("\nTesting getTaskID...");
-        int expResult = 1;
-        int result = treatment1.getTaskID();
-        assertEquals("taskID was incorrect: ", expResult, result);
-
-    }
-
-    @Test
-    public void testGetStartHour() {
-
-        Treatment treatment1 = new Treatment(4, 6, 1, 6);
-        
-        System.out.println("\nTesting getStartHour...");
-        int expResult = 1;
-        int result = treatment1.getStartHour();
-        assertEquals("startHour was incorrect: ", expResult, result);
-
-    }
 
     @Test
     public void TestGetNeedTreatment() {
@@ -142,6 +91,57 @@ public class ScheduleTest {
         Species enumValue = Species.valueOf(animalSpecies);
         int actualCageCleanDuration = enumValue.getCageCleanDuration();
         assertEquals(expectedCageCleanDuration, actualCageCleanDuration);
+    }
+    
+    //Basic Tests for Treatment class to be added to main ScheduleTest file
+
+    @Test
+    public void testGetTreatmentID() {
+        
+        Treatment treatment1 = new Treatment(1, 6, 1, 0)
+              
+        System.out.println("\nTesting getTreatmentID...");
+        int expResult = 1;
+        int result = treatment1.getTreatmentID();
+        assertEquals("treatmentID was incorrect: ", expResult, result);
+
+    }
+
+    @Test
+    public void testGetAnimalID() {
+
+        Treatment treatment1 = new Treatment(2, 6, 1, 2);
+
+        System.out.println("\nTesting getAnimalID...");
+        int expResult = 6;
+        int result = treatment1.getAnimaltID();
+        assertEquals("animalID was incorrect: ", expResult, result);
+
+    }
+
+
+    @Test
+    public void testGetTaskID() {
+
+        Treatment treatment1 = new Treatment(3, 6, 1, 4);
+
+        System.out.println("\nTesting getTaskID...");
+        int expResult = 1;
+        int result = treatment1.getTaskID();
+        assertEquals("taskID was incorrect: ", expResult, result);
+
+    }
+
+    @Test
+    public void testGetStartHour() {
+
+        Treatment treatment1 = new Treatment(4, 6, 1, 6);
+        
+        System.out.println("\nTesting getStartHour...");
+        int expResult = 1;
+        int result = treatment1.getStartHour();
+        assertEquals("startHour was incorrect: ", expResult, result);
+
     }
 
 }
