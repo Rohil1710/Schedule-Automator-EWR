781cacbd9666570ce8b8474e871a475121ca2d70
T-Smithy <tanis.smith@ucalgary.ca>
2023-03-22 18:29:51 -0600

    Updates to Treatment class tests

--- TreatmentTest.java
+++ TreatmentTest.java
@@ -3,30 +3,49 @@
 
 
     @Test
-    public void testTreatmentClass() {
+    public void testGetTreatmentID() {
         
-        Treatment treatment1 = new Treatment(int 1, int 6, int 1, int 0)
+        Treatment treatment1 = new Treatment(1, 6, 1, 0)
               
-        System.out.println("Test Treatment Construcor and Getters:\n");
         System.out.println("\nTesting getTreatmentID...");
         int expResult = 1;
         int result = treatment1.getTreatmentID();
-        assertEquals("TreatmentID was incorrect: ", expResult, result);
+        assertEquals("treatmentID was incorrect: ", expResult, result);
+
+    }
+
+    @Test
+    public void testGetAnimalID() {
+
+        Treatment treatment1 = new Treatment(1, 6, 1, 0);
 
         System.out.println("\nTesting getAnimalID...");
         int expResult = 6;
-        int result = treatment1.getTreatmentID();
-        assertEquals("AnimalID was incorrect: ", expResult, result);
+        int result = treatment1.getAnimaltID();
+        assertEquals("animalID was incorrect: ", expResult, result);
+
+    }
+
+
+    @Test
+    public void testGetTaskID() {
+
+        Treatment treatment1 = new Treatment(1, 6, 1, 0);
 
         System.out.println("\nTesting getTaskID...");
         int expResult = 1;
         int result = treatment1.getTaskID();
-        assertEquals("TaskID was incorrect: ", expResult, result);
+        assertEquals("taskID was incorrect: ", expResult, result);
+
+    }
+
+    @Test
+    public void testGetStartHour() {
         
         System.out.println("\nTesting getStartHour...");
         int expResult = 1;
         int result = treatment1.getStartHour();
-        assertEquals("TaskID was incorrect: ", expResult, result);
+        assertEquals("startHour was incorrect: ", expResult, result);
 
     }
 
