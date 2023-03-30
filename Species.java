02d225a0e7feb9d3e892fa4c2984a99e325ae620
T-Smithy <113543505+T-Smithy@users.noreply.github.com>
2023-03-30 14:13:20 -0600

    Added first versions of enumeration files

---
+++ Species.java
@@ -0,0 +1,71 @@
+/**
+@author Tanis Smith <a href="mailto:tanis.smith@ucalgary.ca">
+tanis.smith@ucalgary.ca</a>
+@version 1.1
+@since 1.0
+*/
+
+package edu.ucalgary.oop;
+
+public enum Species{
+	COYOTE,
+	FOX,
+	PORCUPINE,
+	BEAVER,
+	RACCOON;
+	
+	//Returns number of minutes for feeding duration for corresponding species.
+	public int getFeedingDuration(){
+		switch(this){
+			case COYOTE:
+				return 5;	
+			case FOX:
+				return 5;
+			case PORCUPINE:
+				return 5;
+			case BEAVER:
+				return 5;
+			case RACCOON:
+				return 5;
+			default:
+				return 0;
+		}
+	}
+	
+	//Returns number of minutes for food preparation duration for corresponding species.
+	public int getFoodPrepDuration(){
+		switch(this){
+			case COYOTE:
+				return 10;	
+			case FOX:
+				return 5;
+			case PORCUPINE:
+				return 0;
+			case BEAVER:
+				return 0;
+			case RACCOON:
+				return 0;
+			default:
+				return "null";
+		}
+	}
+	
+	
+	//Returns number of minutes for cage cleaning duration for corresponding species.
+	public int getCageCleanDuration(){
+		switch(this){
+			case COYOTE:
+				return 5;	
+			case FOX:
+				return 5;
+			case PORCUPINE:
+				return 10;
+			case BEAVER:
+				return 5;
+			case RACCOON:
+				return 5;
+			default:
+				return "null";
+		}
+	}
+}
