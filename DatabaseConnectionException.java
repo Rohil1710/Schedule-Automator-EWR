b40b6c9d4b25ae097527c25dee6efe99dfaf9a92
mzafari2 <113462468+mzafari2@users.noreply.github.com>
2023-03-31 22:55:01 -0600

    exceptions

---
+++ DatabaseConnectionException.java
@@ -0,0 +1,5 @@
+public class DatabaseConnectionException extends Exception {
+    public DatabaseConnectionException(String error) {
+        super(error);
+    }
+}
---
+++ TimeLimitExceededException.java
@@ -0,0 +1,6 @@
+public class TimeLimitExceededException extends Exception {
+
+    public TimeLimitExceededException(String error) {
+        super(error);
+    }
+}
