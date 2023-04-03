9ad9970063aff51c2eb57b7c381a560303bf9fcd
T-Smithy <tanis.smith@ucalgary.ca>
2023-04-03 16:00:14 -0600

    updated Schedule connection to database

--- edu/ucalgary/oop/Schedule.java
+++ edu/ucalgary/oop/Schedule.java
@@ -7,10 +7,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.*;
-
-// CONSTRUCTOR - retrieves all required data from database and initializes data members to create the schedule.
-
+import java.util.*;    
+    
 public class Schedule {
     private int[] availableTimes = new int[23];
     private boolean[] volunteerNeeded = new boolean[23];
@@ -18,74 +16,72 @@ public class Schedule {
     private ArrayList<ScheduleItem>[] schedule = new ArrayList[23]; 
     private ArrayList<ScheduleItem> treatmentItems = new ArrayList<ScheduleItem>();
     private ArrayList<ScheduleItem> taskItems = new ArrayList<ScheduleItem>();
-
-
-    public Schedule() throws DatabaseConnectionException {
-
-        try {
-            retrieveAnimals();
-            //retrieveTreatments();
-
-        }
     
-        catch (Exception e) {
-            System.out.println(e.getMessage());
-            //e.printStackTrace();
-        }
-
-    }
-
-    public void retrieveAnimals() throws DatabaseConnectionException{
-        try{
+    // CONSTRUCTOR - retrieves all required data from database and initializes data members to create the schedule.
+    public Schedule() throws DatabaseConnectionException {
+    
+        try {
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/EWR", "user1", "ensf");
             Statement statement = connection.createStatement();
+            retrieveAnimals(statement);
+            //retrieveTreatments();
+            connection.close();
+    
+        }
+        
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw new DatabaseConnectionException("Database Error");
+            //e.printStackTrace();
+        }
+    
+    }
+    
+    
+    public void retrieveAnimals(Statement statement) {
+        try{
+                
             ResultSet rs = statement.executeQuery("SELECT * FROM EWR.ANIMALS;");
             while (rs.next()) {
                 int animalID = rs.getInt("AnimalID");
                 String animalNickname = rs.getString("AnimalNickname");
                 String animalSpecies = rs.getString("AnimalSpecies");
-
+    
                 Animal animal = new Animal(animalID, animalNickname, animalSpecies);
                 this.animals.add(animal);
             }
-            connection.close();
         }
-
+    
         catch(SQLException e){
             e.printStackTrace();
         }
-
-        catch(Exception e){
-            throw new DatabaseConnectionException("Connection Error");
-        }
-
-
+    
     }
-
-
-// GETTERS:
-
+    
+    
+    // GETTERS:
+    
     public ArrayList<Animal> getAnimals(){
         return this.animals;
     }
-
+    
     public ArrayList<Animal> getTreatmentItems(){
         return this.animals;
     }
-
+    
     public ArrayList<Animal> getTaskItems(){
         return this.animals;
     }
-
+    
     public int[] getAvailableTimes(){
         return this.availableTimes;
     }
-    
+        
     public boolean[] getVolunteerNeeded(){
         return this.volunteerNeeded;
     }
-
+    
 // Formats the schedule into a single string, creates a .txt file and outputs string version of schedule to terminal.
-//public void formatSchedule(){}
-
+    //public void formatSchedule(){}
+    
 }
