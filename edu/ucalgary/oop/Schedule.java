8a5feba591f9bdb514f3d5ed3d48b3bf57f4e9f9
T-Smithy <tanis.smith@ucalgary.ca>
2023-04-03 13:29:15 -0600

    added queryDatabase()

--- edu/ucalgary/oop/Schedule.java
+++ edu/ucalgary/oop/Schedule.java
@@ -13,7 +13,31 @@ public class Schedule {
     private boolean[] volunteerNeeded = new boolean[23];
     private ArrayList<Animal> animals;
     private ArrayList<ScheduleItem>[] schedule = new ArrayList[23]; 
+    private ArrayList<ScheduleItem> treatmentItems;
+    private ArrayList<ScheduleItem> taskItems;
 
+
+    public Schedule(){
+
+    }
+
+    public ResultSet queryDatabase(String query) throws DatabaseConnectionException{
+
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/EWR", "user1", "ensf");
+            Statement statement = connection.createStatement();
+
+            // Query the database and create ScheduleItem objects
+            // String query = "SELECT TREATMENTS.TreatmentID, TREATMENTS.AnimalID, TREATMENTS.TaskID, TREATMENTS.StartHour, TASKS.MaxWindow, TASKS.Duration, TASKS.Description FROM TREATMENTS INNER JOIN TASKS ON TREATMENTS.TaskID = TASKS.TaskID";
+            ResultSet resultSet = statement.executeQuery(query);
+            connection.close();
+            return resultSet;
+        }
     
+        catch (Exception e) {
+            throw new DatabaseConnectionException("Could not connect to database.")
+            e.printStackTrace();
+        }
 
+    }
 }
