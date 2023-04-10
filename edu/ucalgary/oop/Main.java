0818d17c7664678b0477a6b9fd0ea23b9516faf3
T-Smithy <tanis.smith@ucalgary.ca>
2023-04-09 22:16:32 -0600

    added some documentation

--- edu/ucalgary/oop/Main.java
+++ edu/ucalgary/oop/Main.java
@@ -1,7 +1,19 @@
+/**
+@author Tanis Smith <a href="mailto:tanis.smith@ucalgary.ca">
+tanis.smith@ucalgary.ca</a>
+@version 1.3
+@since 1.0
+*/
+
 package edu.ucalgary.oop;
 
 import java.util.*;   
 
+/**
+    * Method to instantiate a Schedule object for the following day, then saves it as a .txt file via class method.
+    * @param args[] (Not used.)
+    * @return Nothing
+    */
 public class Main {
     public static void main(String[] args) {
         try{
