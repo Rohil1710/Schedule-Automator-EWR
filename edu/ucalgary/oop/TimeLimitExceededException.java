f8d767ae9f988219e06b97c18d5647893a1acf11
Rohil1710 <rohil1710@gmail.com>
2023-04-11 03:41:34 -0600

    FINAL COMMIT

--- edu/ucalgary/oop/TimeLimitExceededException.java
+++ edu/ucalgary/oop/TimeLimitExceededException.java
@@ -1,7 +1,25 @@
+/**
+@author Tanis Smith <a href="mailto:tanis.smith@ucalgary.ca">
+tanis.smith@ucalgary.ca</a>
+@author Rohil Dhillon <a href="mailto:Rohil.Dhillon@ucalgary.ca">
+Rohil.Dhillon@ucalgary.ca</a>
+@author Mehrnaz Zafari <a href="mailto:mehrnaz.zafari@ucalgary.ca">
+mehrnaz.zafari@ucalgary.ca</a>
+@author Abhyudai Singh <a href="mailto:abhyudai.singh@ucalgary.ca">
+abhyudai.singh@ucalgary.ca</a>
+@version 1.2
+@since 1.0
+*/
 package edu.ucalgary.oop;
 
 public class TimeLimitExceededException extends Exception {
 
+    /**
+     * Constructs a new TimeLimitExceededException object with the specified error
+     * message.
+     *
+     * @param error the error message associated with the exception
+     */
     public TimeLimitExceededException(String error) {
         super(error);
     }
--- edu/ucalgary/oop/Type.java
+++ edu/ucalgary/oop/Type.java
@@ -1,6 +1,12 @@
 /**
 @author Tanis Smith <a href="mailto:tanis.smith@ucalgary.ca">
 tanis.smith@ucalgary.ca</a>
+@author Rohil Dhillon <a href="mailto:Rohil.Dhillon@ucalgary.ca">
+Rohil.Dhillon@ucalgary.ca</a>
+@author Mehrnaz Zafari <a href="mailto:mehrnaz.zafari@ucalgary.ca">
+mehrnaz.zafari@ucalgary.ca</a>
+@author Abhyudai Singh <a href="mailto:abhyudai.singh@ucalgary.ca">
+abhyudai.singh@ucalgary.ca</a>
 @version 1.2
 @since 1.0
 */
@@ -12,7 +18,15 @@ public enum Type{
 	DIURNAL,
 	CREPUSCULAR;
 	
-	//Times given in 24-hour format.
+	/**
+	 * Returns the feeding start time for an animal based on its activity pattern.
+	 *
+	 * @return an integer representing the feeding start time for the animal
+	 *
+	 * @throws IllegalArgumentException if the animal's activity pattern is not recognized
+	 *
+	 * @since 1.0
+	 */
 	public int getFeedingStartTime(){
 		switch(this){
 			case NOCTURNAL:
@@ -27,7 +41,7 @@ public enum Type{
 			default:
 				int times = -1;
 				return times;
-				//Maybe throw an error instead?
+				
 		}
 	}
 	
